3e4d96384b758d8fc1b457fbd8345bb71c26dc5e
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-05-11 13:39:36 +0800

    update

---
+++ Activity 5/debug.py
@@ -0,0 +1,8 @@
+def calculate average(numbers):
+    if numbers == []
+    return 0
+total = sum(numbers)
+average = total / len(numbers)
+return average
+
+numbers []
--- mypackage/module_1.py
+++ mypackage/module_1.py
@@ -1,2 +1,25 @@
+from random import randint
+dice_imgs = ["1", "2", "3", "4", "5", "6"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
+
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page == int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+pages = 0
+word_per_page = 0
+print(f"pages: {pages}, word_per_page: {word_per_page}")
+pages = int(input("Number of pages: "))
+print(f"pages after input: {pages}")
+word_per_page = int(input("Number of words per page: "))
+print(f"word_per_page after input: {word_per_page}")
+total_words = pages * word_per_page
+print(total_words)
+       
 def greet (name):
     print(f"Hi {name}!")
+
